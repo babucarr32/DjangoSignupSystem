b2feb32056ff443435750e1a7fbece408f008e63
babucarr32 <baboucarry@gmail.com>
2023-01-22 20:18:54 +0000

    Created forms.py

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class signupModel(models.Model):
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
