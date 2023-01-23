b5a306bf1f781e301ea74502d8c5f0b6da0d0eb5
babucarr32 <baboucarry@gmail.com>
2023-01-23 21:46:04 +0000

    Its working fine

--- signup/urls.py
+++ signup/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import signup
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('signup', signup, name='signup'),
+    path('signup/', signup, name='signup'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- signup/views.py
+++ signup/views.py
@@ -7,9 +7,13 @@ def signup(request):
         if request.POST['password'] == request.POST['password2']:
             try:
                 user = User.objects.get(username = request.POST['username'])
-                return render(request, {"UserExists": "Username has already been taken"})
+                return render(request, 'signup.html', {"UserExists": "Username has already been taken"})
             except User.DoesNotExist:
-                user.objects.create_user(request.POST['username'], request.POST['passsword'])
+                user = User.objects.create_user(request.POST['username'], request.POST['password'])
                 auth.login(request, user)
-                return redirect('/')
-    return render(request, 'signup.html')
+                return render(request, 'welcome.html')
+        else:
+            return render(request, 'signup.html', {"passwordUnmatched": "Password does not match"})
+    else:
+        print("Request not post")
+        return render(request, 'signup.html')
