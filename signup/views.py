58ae0e0c3cc0f84668d5c0dde9135ec33594ab2a
babucarr32 <baboucarry@gmail.com>
2023-01-23 20:05:34 +0000

    Almost done with the sigup function

--- signup/views.py
+++ signup/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, {"UserExists": "Username has already been taken"})
+            except User.DoesNotExist:
+                user.objects.create_user(request.POST['username'], request.POST['passsword'])
+                auth.login(request, user)
+                return redirect('/')
     return render(request, 'signup.html')
